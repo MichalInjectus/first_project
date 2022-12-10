b67bfd6c956a4707e774e41240757abb1f57142a
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 14:50:49 +0100

    nowa_aktu7

--- new_3.py
+++ new_3.py
@@ -7,3 +7,18 @@ print(content)
 for i in range (len(content)):
     content[i] = content[i].split(',')
 print(content)
+
+#obliczanie sredniej wyplaty
+
+total = 0
+for i  in range(1,len(content)):
+    tital+=int(content[i][1])
+average = total / (len(content)-1)
+print(round(average,2))
+
+#ile kobiet na macierzyńskim
+
+for i  in range(1,len(content)):
+    if content[i][3] == 'k' and content[i][4] == 't':
+        total+==1
+print(total)
